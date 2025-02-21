aac2c6ae8a8300ccaa85429777ea5ceb561f885e
Gil Reich <codingr@gmail.com>
2025-02-21 14:54:01 +0200

    got layout to finally work

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,28 @@ struct MainBoardNode;
 struct MainBoardContainerEntity(Entity);
 
 
+#[derive(Component)]
+struct BoardPieceNode(i32, i32);
+
+#[derive(Component)]
+struct BoardVerticalBorder(i32);
+
+#[derive(Component)]
+struct BoardHorizontalBorder(i32);
+
+struct BoardPieceTransform  {
+    width: f32,
+    height: f32,
+    x: f32,
+    y: f32,
+}
+
+#[derive(Event)]
+struct EventBoardDimensionsChanged {
+    dimension: f32,
+    node: Node,
+}
+
 pub struct TicTacToe;
 
 const BG_COLOR: Color = Color::srgba(0.40, 0.01, 0.2, 0.15);
@@ -26,15 +48,92 @@ impl Plugin for TicTacToe {
         app
         .insert_resource(BoardDimension(0.0))
         .add_systems(Startup, (find_board_dimension, setup_board, setup_pieces).chain())
-        .add_systems(Update, on_window_resize);
+        .add_systems(Update, on_window_resize)
+        .add_systems(Update, (
+            on_board_dimension_change_boarders_x, 
+            on_board_dimension_change_boarders_y,
+            on_board_dimension_change_squares,
+        ))
+        .add_event::<EventBoardDimensionsChanged>();
     }
 }
-fn on_window_resize(mut resize_events: EventReader<WindowResized>, mut main_board_node: Query<&mut Node,With<MainBoardNode>>) {
+fn on_window_resize(
+    mut resize_events: EventReader<WindowResized>, 
+    mut event_writer: EventWriter<EventBoardDimensionsChanged>,
+    mut main_board_node: Query<&mut Node, With<MainBoardNode>>,
+    // mut board_borders_x: Query<(Entity, &mut Node), (With<BoardVerticalBorder>, Without<MainBoardNode>)>,
+    // mut board_borders_y: Query<(Entity, &mut Node), (With<BoardHorizontalBorder>, Without<MainBoardNode>)>,
+    //mut squares_params_set: Query<&mut Node,With<BoardPieceNode>>,
+) {
     if let Some(event) = resize_events.read().last() {
         let dim = f32::min(event.width, event.height);
         if let Some(mut board) = main_board_node.iter_mut().next() {
+            println!("board dim changed, {}", dim);
             board.width = Val::Px(dim);
             board.height = Val::Px(dim);
+            event_writer.send(EventBoardDimensionsChanged { dimension: dim, node: board.to_owned() });
+        }
+    }
+        // let mut node_updates = Vec::new();
+        // for (board_piece, entity) in squares_params_set.p0().iter() {
+        //     let square_transform = get_square_transform(dim, board_piece.0, board_piece.1);
+        //     node_updates.push((entity, square_transform));
+        // }
+        // for (entity, square_transform) in node_updates {
+        //     if let Ok(mut node) = squares_params_set.p1().get_mut(entity) {
+        //         node.width = Val::Px(square_transform.width);
+        //         node.height = Val::Px(square_transform.height);
+        //         node.left = Val::Px(square_transform.x);
+        //         node.right = Val::Px(square_transform.y);
+        //     }
+        // }
+}
+
+fn on_board_dimension_change_boarders_x(
+    mut dimension_changed_events: EventReader<EventBoardDimensionsChanged>, 
+    mut board_borders_x: Query<&mut Node, With<BoardVerticalBorder>>,
+) {
+    if let Some(event) = dimension_changed_events.read().last() {
+        let dim = event.dimension;
+        let mut i = 1;
+        for mut board_border in board_borders_x.iter_mut() {
+                board_border.height = Val::Px(dim);
+                board_border.width = Val::Px(get_board_border_width(dim));
+                board_border.left = Val::Px(get_board_border_position_factor(dim, i));
+                i += 1;
+        }
+    }
+}
+
+
+fn on_board_dimension_change_boarders_y(
+    mut dimension_changed_events: EventReader<EventBoardDimensionsChanged>, 
+    mut board_borders_y: Query<&mut Node, With<BoardHorizontalBorder>>,
+) {
+    if let Some(event) = dimension_changed_events.read().last() {
+        let dim = event.dimension;
+        let mut i = 1;
+        for mut board_border in board_borders_y.iter_mut() {
+            board_border.width = Val::Px(dim);
+            board_border.height = Val::Px(get_board_border_height(dim));
+            board_border.top = Val::Px(get_board_border_position_factor(dim, i));
+            i += 1;
+        }
+    }
+}
+
+fn on_board_dimension_change_squares(
+    mut dimension_changed_events: EventReader<EventBoardDimensionsChanged>, 
+    mut squares_q: Query<(&mut Node, &BoardPieceNode),With<BoardPieceNode>>,
+) {
+    if let Some(event) = dimension_changed_events.read().last() {
+        let dim = event.dimension;
+        for (mut node, piece) in &mut squares_q {
+            let square_transform = get_square_transform(dim, piece.0, piece.1);
+            node.width = Val::Px(square_transform.width);
+            node.height = Val::Px(square_transform.height);
+            node.left = Val::Px(square_transform.x);
+            node.top = Val::Px(square_transform.y);
         }
     }
 }
@@ -50,39 +149,44 @@ fn find_board_dimension(mut windows: Query<&mut Window>, mut board_dimension: Re
     }
 }
 
+fn get_square_transform(board_dimension: f32, x: i32, y: i32) -> BoardPieceTransform {
+    let rect_dim = board_dimension / 3.0;
+    let lines_width =  board_dimension * LINES_WIDTH_PERCENTAGE * 0.01;
+    let lines_width_half = lines_width * 0.5;
+    let x_f32: f32 = x as f32;
+    let y_f32 = y as f32;
+    let left: f32 =  board_dimension  * (THIRD * x_f32 * 0.01) +  ( if x == 1 { 1.0 } else if x == 2 { 3.0 } else { 0.0 }) * lines_width_half;
+    let top: f32 = board_dimension  * (THIRD * y_f32 * 0.01) +  ( if y == 1 { 1.0 } else if y == 2 { 3.0 } else { 0.0 }) * lines_width_half;
+    return BoardPieceTransform {
+        width: rect_dim  - if x == 0 { lines_width_half } else if x == 1 { 0.0 } else { lines_width },
+        height: rect_dim  - if y == 0 { lines_width_half } else if y == 1 { 0.0 } else { lines_width },
+        x: left,
+        y: top,
+    };
+}
+ 
 fn setup_pieces(
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
     mut commands: Commands,
     board_query: Option<Res<MainBoardContainerEntity>>,
     board_dimension: Res<BoardDimension>,
 ) {
-    let rect_dim = board_dimension.0 / 3.0;
-    let lines_width =  board_dimension.0 * LINES_WIDTH_PERCENTAGE * 0.01;
-    let lines_width_half = lines_width * 0.5;
-    let rectangle_asset = meshes.add(Rectangle::default());
-    let material_asset = materials.add(Color::from(PURPLE));
     if let Some(board) = board_query {
         commands.entity(board.0).with_children(|parent| {
             for i in 0..=2 {
                 for j in 0..=2 {
-                    let i_f32: f32 = i as f32;
-                    let j_f32 = j as f32;
-                    let x_fac: f32 =  board_dimension.0  * (THIRD * i_f32 * 0.01) +  ( if i == 1 { 1.0 } else if i == 2 { 2.0 } else { 0.0 }) * lines_width;
-                    let y_fac: f32 =  board_dimension.0  * (THIRD * j_f32 * 0.01) +  ( if j == 1 { 1.0 } else if j == 2 { 2.0 } else { 0.0 }) * lines_width;
-                    println!("For i={}, j={}, then x={}, y={} and scale is {}", i_f32, j_f32, x_fac, y_fac, rect_dim );
+                    let board_piece_transform = get_square_transform(board_dimension.0, i, j);
                     parent.spawn((
-                        //ImageBundle,
+                        BoardPieceNode(i, j),
                         Node {
-                            width: Val::Px(rect_dim  - if i == 1 { lines_width } else { lines_width_half }),
-                            height: Val::Px(rect_dim - if j == 1 { lines_width } else { lines_width_half }),
+                            width: Val::Px(board_piece_transform.width),
+                            height: Val::Px(board_piece_transform.height),
                             position_type: PositionType::Absolute,
-                            top: Val::Px(y_fac),
+                            top: Val::Px(board_piece_transform.y),
                             border: UiRect { left: Val::Px(1.0), right: Val::Px(1.0), top: Val::Px(1.0), bottom: Val::Px(1.0), },
-                            left: Val::Px(x_fac),
+                            left: Val::Px(board_piece_transform.x),
                             ..default()
                         },
-                        BackgroundColor(PURPLE),
+                        BackgroundColor(PURPLE), 
                         BorderColor(Color::from(BLACK)),
                     ));
                 }
@@ -92,11 +196,28 @@ fn setup_pieces(
     }
 }
 
+
+fn get_board_border_position_factor(board_dimension:f32, i: i32) -> f32 {
+    let i_float = i as f32;
+    let lines_width =  board_dimension * LINES_WIDTH_PERCENTAGE * 0.01;
+    let lines_width_half =  lines_width * 0.5;
+    let position_factor: f32 =  board_dimension * (THIRD * i_float * 0.01)  +  ( if i == 1 { -1.0 } else { 1.0 }) * lines_width_half; 
+
+    return position_factor;
+}
+
+fn get_board_border_width(board_dimension: f32) -> f32 {
+    LINES_WIDTH_PERCENTAGE * board_dimension * 0.01
+}
+
+fn get_board_border_height(board_dimension: f32) -> f32 {
+    LINES_WIDTH_PERCENTAGE * board_dimension * 0.01
+}
+
+
 fn setup_board(
-    mut commands: Commands, 
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    mut board_dimension: Res<BoardDimension>,
+    mut commands: Commands,
+    board_dimension: Res<BoardDimension>,
 ) {
     commands.spawn(Camera2d);
     let board_dim = board_dimension.0;
@@ -105,45 +226,40 @@ fn setup_board(
             Node {
                 width: Val::Px(board_dim),
                 height: Val::Px(board_dim),
-                align_items: AlignItems::Center,
-                align_self: AlignSelf::Center, 
                 aspect_ratio: Some(1.0),
                 ..default()
             },
             BackgroundColor(BG_COLOR),
     ));
     let main_board_node_id = main_board_node.id();
-    main_board_node.with_children(|parent| { 
-        for i in 1..=2 {
-            let i_float = i as f32;
-            let lines_width =  board_dim * LINES_WIDTH_PERCENTAGE * 0.01;
-            let lines_width_half =  lines_width * 0.5;
-            let position_factor: f32 =  board_dim * (THIRD * i_float * 0.01)  +  ( if i == 1 { -1.0 } else { 1.0 }) * lines_width_half; 
-            println!("HO {}", lines_width);
-            println!("HI {}", position_factor);
-            parent.spawn((
-                Node {
-                    width: Val::Px(LINES_WIDTH_PERCENTAGE * board_dim * 0.01),
-                    height: Val::Px(board_dim),
-                    left: Val::Px(position_factor),
-                    position_type: PositionType::Absolute,
-                    ..default()
-                },
-                BackgroundColor(BG_LINES),
-            ));
-            parent.spawn((
-                Node {
-                    width: Val::Px(board_dim),
-                    height: Val::Px(LINES_WIDTH_PERCENTAGE * board_dim * 0.01),
-                    top: Val::Px(position_factor),
-                    position_type: PositionType::Absolute,
-                    ..default()
-                    
-                },
-                BackgroundColor(BG_LINES),
-            ));
-        }
-    });
+    for i in 1..=2 {
+        let position_factor = get_board_border_position_factor(board_dim, i);
+        let board_border_vertical_width = get_board_border_width(board_dim);
+        let board_border_horizontal_height = get_board_border_height(board_dim);
+        commands.spawn((
+            BoardVerticalBorder(i),
+            Node {
+                width: Val::Px(board_border_vertical_width),
+                height: Val::Px(board_dim),
+                left: Val::Px(position_factor),
+                position_type: PositionType::Absolute,
+                ..default()
+            },
+            BackgroundColor(BG_LINES),
+        ));
+        commands.spawn((
+            BoardHorizontalBorder(i),
+            Node {
+                width: Val::Px(board_dim),
+                height: Val::Px(board_border_horizontal_height),
+                top: Val::Px(position_factor),
+                position_type: PositionType::Absolute,
+                ..default()
+                
+            },
+            BackgroundColor(BG_LINES),
+        ));
+    }
 
     commands.insert_resource(MainBoardContainerEntity(main_board_node_id));
 
